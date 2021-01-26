76b651d3737a0a070985e213b7cb8ce8a52d6c94
Cossack Proger <yiksnelots@gmail.com>
2021-01-26 21:57:11 +0200

    checking heroku db

--- src/main/java/ua/nic/landing/controller/IndexViewController.java
+++ src/main/java/ua/nic/landing/controller/IndexViewController.java
@@ -1,14 +1,21 @@
 package ua.nic.landing.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.nic.landing.model.ProductEntity;
+import ua.nic.landing.repository.IProductRepository;
 
 @Controller
 public class IndexViewController {
+	@Autowired
+	private IProductRepository productRepository;
 	@GetMapping("/")
 	public String startPage (Model model) {
-		model.addAttribute("productCount", 5);
+		model.addAttribute("productCount", productRepository.findAll().size());
+		ProductEntity newProduct = new ProductEntity();
+		productRepository.save(newProduct);
 		return "index.html";
 	}
 }
