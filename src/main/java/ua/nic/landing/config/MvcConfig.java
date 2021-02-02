72e9994f3e3d2f6d8fb0b1793a56e4f497426fb6
Cossack Proger <yiksnelots@gmail.com>
2021-02-02 10:58:26 +0200

    fixed path

--- src/main/java/ua/nic/landing/config/MvcConfig.java
+++ src/main/java/ua/nic/landing/config/MvcConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers (ResourceHandlerRegistry registry) {
+        File file = new File("pom.xml");
+        String absolutePath = file.getAbsolutePath().replaceAll("\\\\", "/");
+        absolutePath = absolutePath.replaceAll("pom\\.xml", "");
         registry.addResourceHandler("/image_db/**")
-                .addResourceLocations("file:///src/main/resources/image_db/");
+                .addResourceLocations("file:///" + absolutePath + "src/main/resources/image_db/");
     }
 }
